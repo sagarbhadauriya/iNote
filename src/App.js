7cdae0e45ed7f4a83d3fc50c0aba5868a21fda18
sagarbhadauriya <sagarbhadauriya96@gmail.com>
2023-04-28 23:08:49 +0530

    updated app file commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,43 @@
-import React, { useState, memo } from 'react';
-import PropTypes from 'prop-types';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
-// Single List Item
-const WrappedSingleListItem = ({
-  isSelected,
-  onClickHandler,
-  text,
-}) => {
+function App() {
   return (
-    <li
-      style={{ backgroundColor: isSelected ? 'green' : 'red' }}
-      onClick={onClickHandler}
-    >
-      {text}
-    </li>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
-};
+}
 
-WrappedSingleListItem.propTypes = {
-  isSelected: PropTypes.bool,
-  onClickHandler: PropTypes.func,
-  text: PropTypes.string.isRequired,
-};
-
-const SingleListItem = memo(WrappedSingleListItem);
-
-// List Component
-const WrappedListComponent = ({
-  items,
-}) => {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-
-  const handleClick = index => {
-    setSelectedIndex(index);
-  };
-
-  return (
-    <ul style={{ textAlign: 'left' }}>
-      {items.map((item) => (
-        <SingleListItem
-          key={item.itemId}
-          onClickHandler={() => handleClick(item.itemId)}
-          text={item.text}
-          isSelected={selectedIndex === item.itemId}
-        />
-      ))}
-    </ul>
-  );
-};
-
-WrappedListComponent.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      itemId: PropTypes.number.isRequired
-    }),
-  ),
-};
-
-WrappedListComponent.defaultProps = {
-  items: [
-    { text: 'List 1', itemId: 1},
-    { text: 'List 2', itemId: 2},
-    { text: 'List 3', itemId: 3},
-    { text: 'List 4', itemId: 4},
-    { text: 'List 5', itemId: 5},
-  ],
-};
-
-const List = memo(WrappedListComponent);
-
-export default List;
+export default App;
